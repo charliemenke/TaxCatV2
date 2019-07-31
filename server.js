435a3e640d26a5ab94b01999e47aa76246a0b722
Charlie Menke <charlie.menke@arrow.com>
2019-07-31 09:55:23 -0600

    prev

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const amqp = require('amqplib');
 
 const port = process.env.PORT || 3000;
-const open = amqp.connect("amqp://guest:8R1LKbvyHa71@localhost:5672");
+const open = amqp.connect("amqp://guest:guest@localhost:5672");
 const exchangeName = "analysePostID";
 const queueName = "postIDQueue";
 require('dotenv').config();
