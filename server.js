6919eb26373a329953422b0eaad9939199eb442d
Charlie Menke <charlie.menke@arrow.com>
2019-08-01 16:06:58 -0600

    +4

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ async function checkPostIDValilidy(message) {
 			let bodyStr = body.toString().substring(body.toString().indexOf('{'));
 			//bodyStr = bodyStr.substr(0,bodyStr.lastIndexOf("}") + 1);
 			//console.log(bodyStr);
-			bodyStr = JSON.parse(bodyStr);
+			bodyStr = JSON.stringify(bodyStr);
 			console.log(bodyStr);
 		
 				if(body.id) {
